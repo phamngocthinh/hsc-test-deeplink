7859b9524881ed8ced1d243276ab77777776ea71
Thinh. Pham Ngoc <thinh.pn@hsc.com.vn>
2025-09-24 18:02:25 +0700

    Update redirectToDeviceBrowser.ts

--- src/utils/redirectToDeviceBrowser.ts
+++ src/utils/redirectToDeviceBrowser.ts
@@ -1,25 +1,31 @@
-export const redirectToDeviceBrowser = async ({ url, os }: { url: string; os: 'android' | 'ios' }) => {
-  if (typeof window === 'undefined') {
+export const redirectToDeviceBrowser = async ({
+  url,
+  os,
+}: {
+  url: string;
+  os: "android" | "ios";
+}) => {
+  if (typeof window === "undefined") {
     return;
   }
 
-  const urlWithoutProtocol = url.replace(/^https?:\/\//, '');
+  const urlWithoutProtocol = url.replace(/^https?:\/\//, "");
 
   try {
-    if (os === 'ios') {
+    if (os === "ios") {
       // Try safari - 15, 17, 18
       const iosUrl = `x-safari-${url}`;
-      window.location.href = iosUrl;
-
-      // Try safari old way
-      await new Promise((r) => setTimeout(r, 1000));
-      const iosOldUrl = `com-apple-mobilesafari-tab:${url}`;
       window.location.href = "hscone://open/profile?id=123";
+
+      // // Try safari old way
+      // await new Promise((r) => setTimeout(r, 1000));
+      // const iosOldUrl = `com-apple-mobilesafari-tab:${url}`;
+      // window.location.href = "hscone://open/profile?id=123";
 
       return false;
     }
 
-    if (os === 'android') {
+    if (os === "android") {
       const androidIntent = `intent://${urlWithoutProtocol}#Intent;scheme=http;package=com.android.chrome;end;`;
       window.location.href = androidIntent;
       return false;
