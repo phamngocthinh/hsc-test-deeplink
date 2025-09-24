43e2347ce04129fb2ac80d9ad1ec8d7ed82ae77a
Thinh. Pham Ngoc <thinh.pn@hsc.com.vn>
2025-09-24 17:57:27 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import GlobalSetup from "./components/GlobalSetup";
 import ExternalPage from "./components/ExternalPage";
 
+// <ExternalPage
+//   src="https://register-ibs-uat.hsc.com.vn/"
+//   title="HSC Register"
+// />
 function App() {
   return (
     <div className="App">
-      <ExternalPage
-        src="https://register-ibs-uat.hsc.com.vn/"
-        title="HSC Register"
-      />
       <GlobalSetup />
     </div>
   );
